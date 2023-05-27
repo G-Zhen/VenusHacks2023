6df3b6c9996dec4480576a79f5c92b72fd58fb81
Sabrina Tang <sabrit4@uci.edu>
2023-05-27 10:38:55 -0700

    Results Class stores namedtuple of all recipes

--- BackEnd/json_formatting.py
+++ BackEnd/json_formatting.py
@@ -1,13 +1,18 @@
 import requests
-import json 
+import json
+from collections import namedtuple
 
+Item = namedtuple("Item", ['name' 'image' 'link' 'content'])
 
 class Results:
     def __init__(self, response):
         self._results = response.json()['searchResults']
-
+        self._recipes = []
 
 
     def parse(self):
         recipes = self._results[0]['results']
-        print(recipes)
+        for recipe in recipes:
+            item = Item([recipe['name'], recipe['image'], recipe['link'], recipe['content']])
+            self._recipes.append(item)
+
