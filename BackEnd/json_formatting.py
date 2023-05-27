c544df85f68f49ebaabef133c75bb13a6f23a782
Sabrina Tang <sabrit4@uci.edu>
2023-05-27 11:10:56 -0700

    changed parse function to store data

--- BackEnd/json_formatting.py
+++ BackEnd/json_formatting.py
@@ -1,18 +1,13 @@
-import requests
-import json
 from collections import namedtuple
-
-Item = namedtuple("Item", ['name' 'image' 'link' 'content'])
 
 class Results:
     def __init__(self, response):
         self._results = response.json()['searchResults']
-        self._recipes = []
+        self._recipes = {}
 
 
     def parse(self):
         recipes = self._results[0]['results']
         for recipe in recipes:
-            item = Item([recipe['name'], recipe['image'], recipe['link'], recipe['content']])
-            self._recipes.append(item)
+            self._recipes[recipe['name']] = [recipe['image'], recipe['link'], recipe['content']]
 
