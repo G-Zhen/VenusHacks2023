bb8551826327d3541b5a57b5fc5efb26962fa2b6
Grace Zhen <grace.zhen00@gmail.com>
2023-05-27 11:24:14 -0700

    app.py flask is running with test dictionary

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -2,7 +2,7 @@
 #2) go to app.py in terminal: cd BackEnd
 #3) run file: flask run
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, Response
 from json_formatting import Results
 
 app = Flask(__name__)
@@ -11,28 +11,30 @@ APIpass = "5456LILYAc2024"
 APIkey = "07a51a8ae2df4713b2c2fbe8b9607f50"
 API_URL = "https://api.spoonacular.com/food/search"
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return jsonify({"name": "meal"}) #our index is homepage.html
 
-# @app.route('/search', methods=['POST'])
-# def search():
-#     query = request.form['query']  # Get the query from the form
-#     params = {
-#         "apiKey": APIkey,
-#         "query": query,
-#         "number": 10
-#     }
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']  # Get the query from the form
+    params = {
+        "apiKey": APIkey,
+        "query": query,
+        "number": 10
+    }
 
-#     response = requests.get(API_URL, params=params)
+    response = requests.get(API_URL, params=params)
 
-#     if response.status_code == 200:
-#         results = Results(response)
-#         results.parse()
-#         recipes = results._recipes
-#         return render_template('results.html', recipes=recipes)
-#     else:
-#         return "Error occurred. Status code: " + str(response.status_code)
+    if response.status_code == 200:
+        results = Results(response)
+        results.parse()
+        recipes = results._recipes
+        return jsonify(recipes)
+    else:
+        errorResponse = jsonify({"detail": "Error occurred. Status code: " + str(response.status_code)})
+        errorResponse.status = response.status_code
+        return errorResponse
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
