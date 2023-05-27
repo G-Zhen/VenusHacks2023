b38530f784d7f734ce8648a399d2327709912ce3
Grace Zhen <grace.zhen00@gmail.com>
2023-05-27 09:05:11 -0700

    Set up api.py requests. Code 200 means good to go

--- BackEnd/api.py
+++ BackEnd/api.py
@@ -1,7 +1,18 @@
-#file to implement the api
-
-
-
+#First go to terminal and install: pip install requests
+import requests
+# API info
 #lchakhoy@uci.edu
 #Pass: #5456LILYAc2024
 # api key: 07a51a8ae2df4713b2c2fbe8b9607f50
+
+APIpass = "5456LILYAc2024"
+APIkey = "07a51a8ae2df4713b2c2fbe8b9607f50"
+API_URL = "https://api.spoonacular.com/food/search"
+
+params = {
+    "apiKey": APIkey
+}
+
+response = requests.get(API_URL, params=params)
+print(response.status_code)
+
